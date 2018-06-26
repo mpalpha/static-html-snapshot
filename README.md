b0f0c52021e06d12d529729aa061e20ea704e988
Jason Lusk <jason@jasonlusk.com>
2018-06-26 13:06:52 -0500

    refactor plugin, add deploy task.

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ Create a static snapshot of your WordPress site.
 
 ## Composer
 
-- Run `composer install` and that should be it.
+- Run `composer install --no-ansi --no-dev --no-interaction --no-progress --no-scripts --optimize-autoloader` and that should be it.
+
+## Screenshot
+
+![Screenshot](/screenshot.jpg?raw=true "Screenshot")
--- plugin-slug.php
+++ plugin-slug.php
@@ -13,7 +13,7 @@
  * Plugin Name:       Static HTML Snapshot
  * Plugin URI:        http://localhost
  * Description:       Create a static version of your WordPress site.
- * Version:           1.0.0
+ * Version:           1.3.0
  * Author:            Jason Lusk, Anthony Allen
  * Author URI:        http://jasonlusk.com
  * Text Domain:       static-snapshot
